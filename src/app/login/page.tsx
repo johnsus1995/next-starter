8e8688701cc979eb64090f3c27df095251814ab2
Jaison JOhn <jaisonjohny26@gmail.com>
2024-03-07 22:51:58 +0530

    test: husky works

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
 export default function page() {
-  return <div>Page sdadasd adasd adasda adasd sdd adas adasd</div>;
+  return <div>Page sdadasd adasd adasda adasd adasdsdd adas adasd</div>;
 }
