d78c0eede2635e2e2fb617b7ec72bb827901a2b4
Jaison JOhn <jaisonjohny26@gmail.com>
2024-03-07 22:52:58 +0530

    test: husky test after removing jest

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
 export default function page() {
-  return <div>Page sdadasd adasd adasda adasd adasdsdd adas adasd</div>;
+  return <div>Page sdadasd adasd adasda adasd adasd asda sdsdd adas adasd</div>;
 }
